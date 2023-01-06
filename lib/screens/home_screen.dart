a17c334a5cc85beade9721a97cc58b0e1cb9456d
Pornthawee Thaweesin <pornthawee.t@linecorp.com>
2023-01-07 01:00:21 +0700

    Create Widget Input Text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final nameController = TextEditingController();
+  final surnameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Text('');
+    return Scaffold(
+      appBar: AppBar(title: Text('ลงทะเบียน')),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              buildTextField(),
+              ElevatedButton(
+                onPressed: () {
+                  print('Name: ${nameController.text} ${surnameController.text}');
+                },
+                child: Text('บันทึก'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  Widget buildTextField() => Column(
+    children: [
+      TextField(
+        decoration: InputDecoration(labelText: 'ชื่อ'),
+        maxLength: 50,
+        keyboardType: TextInputType.name,
+        controller: nameController,
+      ),
+      TextField(
+        decoration: InputDecoration(labelText: 'นามสกุล'),
+        maxLength: 50,
+        keyboardType: TextInputType.name,
+        controller: surnameController,
+      ),
+    ],
+  );
 }
